fefcafca1b4dded9f658490374f507d4101176aa
Emilijus <slibinas2012@gmail.com>
2018-10-01 16:09:24 +0300

    Added coins with sound.

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickuypSFX;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AudioSource.PlayClipAtPoint(coinPickuypSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+
+}
